40f9580fdedfbe7c1df3533ce36edecd14af8d4d
Indumathi1195R <54974849+Indumathi1195R@users.noreply.github.com>
2021-04-20 07:09:57 +0530

    Update main.dart
    Sample updated.

--- lib/main.dart
+++ lib/main.dart
@@ -24,28 +24,17 @@ class MyApp extends StatefulWidget {
 }
 
 class ScheduleExample extends State<MyApp> {
-  String _headerText;
-  double _width, _cellWidth;
-  List<String> _dates, _days;
-  bool _selected;
-  int _selectedIndex;
-
-  @override
-  void initState() {
-    _headerText = 'Header';
-    _days = <String>['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-    _dates = <String>['1', '2', '3', '4', '5', '6', '7'];
-
-    _width = 0.0;
-    _cellWidth = 0.0;
-    _selected = false;
-    super.initState();
-  }
+  String? _headerText = 'Header';
+  double? _width = 0.0, _cellWidth = 0.0;
+  List<String>? _dates = <String>['1', '2', '3', '4', '5', '6', '7'],
+      _days = <String>['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  bool? _selected = false;
+  int? _selectedIndex;
 
   @override
   Widget build(BuildContext context) {
     _width = MediaQuery.of(context).size.width;
-    _cellWidth = _width / 8;
+    _cellWidth = _width! / 8;
     return Scaffold(
         body: SafeArea(
       child: Column(
@@ -55,53 +44,55 @@ class ScheduleExample extends State<MyApp> {
             width: _width,
             height: _cellWidth,
             child: Center(
-              child: Text(_headerText,
+              child: Text(_headerText!,
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 25, color: Colors.white)),
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(left: _cellWidth,),
+            padding: EdgeInsets.only(
+              left: _cellWidth!,
+            ),
             child: SizedBox(
               height: _cellWidth,
               child: ListView.builder(
                   shrinkWrap: true,
-                  itemCount: _days.length,
+                  itemCount: _days!.length,
                   scrollDirection: Axis.horizontal,
                   itemBuilder: (BuildContext context, int index) {
-                    return  GestureDetector(
-                        child: Container(
-                          child: Column(
-                            children: [
-                              Container(
-                                height: _cellWidth,
-                                child: Center(
-                                  child: Text(
-                                    _days[index],
-                                    style: TextStyle(
-                                      color: _selected && _selectedIndex == index
-                                          ? Colors.orange
-                                          : Colors.black,
-                                    ),
-                                    textAlign: TextAlign.center,
+                    return GestureDetector(
+                      child: Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              height: _cellWidth,
+                              child: Center(
+                                child: Text(
+                                  _days![index],
+                                  style: TextStyle(
+                                    color: _selected! && _selectedIndex == index
+                                        ? Colors.orange
+                                        : Colors.black,
                                   ),
+                                  textAlign: TextAlign.center,
                                 ),
-                                width: _cellWidth,
                               ),
-                            ],
-                          ),
+                              width: _cellWidth,
+                            ),
+                          ],
                         ),
-                      );
+                      ),
+                    );
                   }),
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(left: _cellWidth),
+            padding: EdgeInsets.only(left: _cellWidth!),
             child: SizedBox(
-              height: _cellWidth / 2,
+              height: _cellWidth! / 2,
               child: ListView.builder(
                   shrinkWrap: true,
-                  itemCount: _dates.length,
+                  itemCount: _dates!.length,
                   scrollDirection: Axis.horizontal,
                   itemBuilder: (BuildContext context, int index) {
                     return GestureDetector(
@@ -116,12 +107,12 @@ class ScheduleExample extends State<MyApp> {
                           children: [
                             Container(
                               child: Text(
-                                _dates[index],
+                                _dates![index],
                                 style: TextStyle(color: Colors.black),
                                 textAlign: TextAlign.center,
                               ),
                               width: _cellWidth,
-                              decoration: _selected && _selectedIndex == index
+                              decoration: _selected! && _selectedIndex == index
                                   ? BoxDecoration(
                                       color: Colors.orange,
                                       shape: BoxShape.circle,
@@ -142,12 +133,12 @@ class ScheduleExample extends State<MyApp> {
               viewHeaderHeight: 0,
               view: CalendarView.week,
               onViewChanged: (ViewChangedDetails viewChangedDetails) {
-                SchedulerBinding.instance.addPostFrameCallback((duration) {
+                SchedulerBinding.instance!.addPostFrameCallback((duration) {
                   for (int i = 0;
                       i < viewChangedDetails.visibleDates.length;
                       i++) {
                     setState(() {
-                      _dates[i] = DateFormat('dd')
+                      _dates![i] = DateFormat('dd')
                           .format(viewChangedDetails.visibleDates[i])
                           .toString();
                       _headerText = DateFormat('MMMM yyyy')
